27238dda982cc4cd2b0ec9fb3bd21262fd0f16a1
lhamo07 <tenlhamo8262@gmail.com>
2022-08-10 14:26:24 +0545

    commit

---
+++ amazon/component/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <header>header</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default Layout;
